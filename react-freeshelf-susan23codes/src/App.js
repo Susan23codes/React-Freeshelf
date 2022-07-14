daa6a28af94cccc51e9ad6445dfdbeea4ed5bd80
Susan <susan23codes@gmail.com>
2022-07-14 12:31:25 -0400

    Initial commit

---
+++ react-freeshelf-susan23codes/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import BookTitle from './BookTitle'
+
+function App() {
+  return (
+    <>
+      <h1>Hello React!</h1>
+      <BookTitle />
+    </>
+  );
+}
+
+export default App;
---
+++ react-freeshelf-susan23codes/src/BookTitle.js
@@ -0,0 +1,14 @@
+export default function BookTitle() {
+    return <h2>Here is the title</h2>
+}
+
+  const books = {
+    "title": "You Don't Know JS Yet: Getting Started",
+    "author": "Kyle Simpson",
+    "url": "https://github.com/getify/You-Dont-Know-JS/tree/2nd-ed/get-started",
+    "shortDescription": "If you're just getting into programming and/or JavaScript, this book will briefly explore what you need to get up and going.",
+    "coverImageUrl": "https://github.com/getify/You-Dont-Know-JS/raw/2nd-ed/get-started/images/cover.png",
+    "publisher": "Frontend Masters",
+    "publicationDate": "Jan 2020",
+    "detailedDescription": "It’s easy to learn parts of JavaScript, but much harder to learn it completely—or even sufficiently—whether you’re new to the language or have used it for years. With the 'You Don’t Know JS' book series, you’ll get a more complete understanding of JavaScript, including trickier parts of the language that many experienced JavaScript programmers simply avoid. The series’ first book, Up & Going, provides the necessary background for those of you with limited programming experience. By learning the basic building blocks of programming, as well as JavaScript’s core mechanisms, you’ll be prepared to dive into the other, more in-depth books in the series—and be well on your way toward true JavaScript."
+  }
